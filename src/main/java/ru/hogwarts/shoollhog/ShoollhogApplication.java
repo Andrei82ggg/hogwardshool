2787d08898cdc8dfe10ed55710a5140b05346c92
Андрей <ustu1982@mail.ru>
2023-11-28 05:11:20 +0300

    one

---
+++ src/main/java/ru/hogwarts/shoollhog/ShoollhogApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.shoollhog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoollhogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoollhogApplication.class, args);
+	}
+
+}
