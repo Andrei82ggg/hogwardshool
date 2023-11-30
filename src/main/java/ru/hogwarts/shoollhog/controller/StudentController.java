b2eb5c992ffb9b82e1ff903a4a46a95adfe92421
Андрей <ustu1982@mail.ru>
2023-11-30 14:42:13 +0300

    prestQL

---
+++ src/main/java/ru/hogwarts/shoollhog/controller/StudentController.java
@@ -0,0 +1,37 @@
+package ru.hogwarts.shoollhog.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.shoollhog.model.Student;
+import ru.hogwarts.shoollhog.service.StudentService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable Long id){
+        return studentService.getStudent(id);
+    }
+    @PostMapping
+    public Student createStudent(@RequestBody Student student){
+        return studentService.addStudent(student);
+    }
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student){
+        return studentService.updateStudent(id, student);
+    }
+    @DeleteMapping("/{id}")
+    public void removeStudent(@PathVariable Long id){
+        studentService.removeStudent(id);
+    }
+    @GetMapping
+    public Collection<Student> getStudentByAge(@RequestParam Integer age){
+        return studentService.getStudentsByAge(age);
+    }
+}
