732478f3a2cc32f1f27cc862fa3e3fcb63124597
Андрей <ustu1982@mail.ru>
2024-01-16 12:17:57 +0300

    streamsFixed

--- src/main/resources/liquibase/scripts/indices.sql
+++ src/main/resources/liquibase/scripts/indices.sql
@@ -2,5 +2,5 @@
 
 -- changeset andre:1
 
-CREATE INDEX student_name_idx ON student (name);
-CREATE INDEX faculty_name_color_idx ON faculty (name, color);
+CREATE INDEX student2_name_idx ON student (name);
+CREATE INDEX faculty2_name_color_idx ON faculty (name, color);
